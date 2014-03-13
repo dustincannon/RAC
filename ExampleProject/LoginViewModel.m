ff5b71977fd7e35fee5847c7787f722d53730713
Dustin Cannon <dustic@amazon.com>
2014-03-13 10:15:18 -0700

    Simple MVVM test

---
+++ ExampleProject/LoginViewModel.m
@@ -0,0 +1,45 @@
+//
+//  LoginViewModel.m
+//  ExampleProject
+//
+//  Created by Cannon, Dustin on 3/12/14.
+//  Copyright (c) 2014 Cannon, Dustin. All rights reserved.
+//
+
+#import "LoginViewModel.h"
+#import <ReactiveCocoa.h>
+#import <ReactiveCocoa/RACEXTScope.h>
+
+@implementation LoginViewModel
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        RACSignal *emailSignal = RACObserve(self, email);
+        RACSignal *passwordSignal = RACObserve(self, password);
+
+        RAC(self, formIsValid) = [RACSignal combineLatest:@[emailSignal, passwordSignal] reduce:^id(NSString *email, NSString *password){
+            if (([email length] > 0) && ([password length] > 0)) {
+                return @(YES);
+            }
+            return @(NO);
+        }];
+    }
+    return self;
+}
+
+//
+//- (void)setEmail:(NSString *)email
+//{
+////    NSLog(@"email: %@", email);
+//    _email = email;
+//}
+//
+//- (void)setPassword:(NSString *)password
+//{
+////    NSLog(@"password: %@", password);
+//    _password = password;
+//}
+
+@end
