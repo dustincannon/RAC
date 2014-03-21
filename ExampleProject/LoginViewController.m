327a68f986882b447f5af258b81fbc39018cea6e
Dustin Cannon <dustic@amazon.com>
2014-03-21 14:42:26 -0700

    Remove unnecessary code

--- ExampleProject/LoginViewController.m
+++ ExampleProject/LoginViewController.m
@@ -15,23 +15,6 @@
 
 @implementation LoginViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (id)initWithCoder:(NSCoder *)aDecoder
-{
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -73,12 +56,6 @@
     }];
     
     self.signInButton.rac_command = loginCommand;
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
