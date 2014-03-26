d494c0f5879d21bde2a28e5727f68430b6be6943
Dustin Cannon <dustic@amazon.com>
2014-03-26 00:13:52 -0700

    Simplify

--- ExampleProject/LoginViewController.m
+++ ExampleProject/LoginViewController.m
@@ -14,51 +14,20 @@
 {
     [super viewDidLoad];
 
-    self.emailField.delegate = self;
-    self.passwordField.delegate = self;
     self.signInButton.enabled = NO;
     self.statusLabel.hidden = YES;
+    
+    [self.emailField addTarget:self action:@selector(possiblyEnableSignIn) forControlEvents:UIControlEventEditingChanged];
+    [self.passwordField addTarget:self action:@selector(possiblyEnableSignIn) forControlEvents:UIControlEventEditingChanged];
 }
 
-- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+- (void)possiblyEnableSignIn
 {
-    self.statusLabel.hidden = YES;
-
-    if (textField == self.emailField) {
-        // editing email field
-        if (self.passwordField.text.length > 0) {
-            // and password field is not empty
-            if (string.length) {
-                // and we are adding a character
-                self.signInButton.enabled = YES;
-            } else if (self.emailField.text.length == 1) {
-                // deleting last character in email field
-                self.signInButton.enabled = NO;
-            }
-        } else {
-            // password field is empty
-            self.signInButton.enabled = NO;
-        }
+    if (self.emailField.text.length > 0 && self.passwordField.text.length > 0) {
+        self.signInButton.enabled = YES;
+    } else {
+        self.signInButton.enabled = NO;
     }
-    
-    if (textField == self.passwordField) {
-        // editing password field
-        if (self.emailField.text.length > 0) {
-            // and email field is not empty
-            if (string.length) {
-                // and we are adding a character
-                self.signInButton.enabled = YES;
-            } else if (self.passwordField.text.length == 1) {
-                // deleting last character in password field
-                self.signInButton.enabled = NO;
-            }
-        } else {
-            // email field is empty
-            self.signInButton.enabled = NO;
-        }
-    }
-
-    return YES;
 }
 
 - (IBAction)signIn:(id)sender
