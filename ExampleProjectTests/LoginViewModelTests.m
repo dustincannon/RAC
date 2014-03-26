1a3f77ae1370c1a8388e86d00fe836e7845bd82e
Dustin Cannon <dustic@amazon.com>
2014-03-26 10:06:17 -0700

    Remove tests so I can write them in demo

--- ExampleProjectTests/LoginViewModelTests.m
+++ ExampleProjectTests/LoginViewModelTests.m
@@ -30,21 +30,4 @@
     [super tearDown];
 }
 
-- (void)testFormIsValid
-{
-    self.viewModel.email = @"email";
-    self.viewModel.password = @"password";
-    XCTAssertEqual(self.viewModel.loginEnabled, YES, @"");
-}
-
-- (void)testAuthetication
-{
-    self.viewModel.email = @"dustin";
-    self.viewModel.password = @"test123";
-    self.viewModel.loginEnabled = YES;
-    
-    [[self.viewModel.loginCommand execute:nil] asynchronouslyWaitUntilCompleted:nil];
-    XCTAssertEqual(self.viewModel.loginSuccessful, YES, @"");
-}
-
 @end
---
+++ ExampleProjectTests/Tests.m
@@ -0,0 +1,46 @@
+//
+//  Tests.m
+//  ExampleProject
+//
+//  Created by Cannon, Dustin on 3/26/14.
+//  Copyright (c) 2014 Cannon, Dustin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+//- (void)setUp
+//{
+//    [super setUp];
+//    // Put setup code here. This method is called before the invocation of each test method in the class.
+//}
+//
+//- (void)tearDown
+//{
+//    // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    [super tearDown];
+//}
+
+//- (void)testFormIsValid
+//{
+//    self.viewModel.email = @"email";
+//    self.viewModel.password = @"password";
+//    XCTAssertEqual(self.viewModel.loginEnabled, YES, @"");
+//}
+//
+//- (void)testAuthetication
+//{
+//    self.viewModel.email = @"dustin";
+//    self.viewModel.password = @"test123";
+//    self.viewModel.loginEnabled = YES;
+//    
+//    [[self.viewModel.loginCommand execute:nil] asynchronouslyWaitUntilCompleted:nil];
+//    XCTAssertEqual(self.viewModel.loginSuccessful, YES, @"");
+//}
+
+@end
